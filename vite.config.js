4f5f33d4d4993534fbb8578b38cd5b548c0fbf41
Heber Yesid <82123300+HeberYesid@users.noreply.github.com>
2026-03-20 09:08:42 -0500

    feat(config): add warmup configuration and optimize dependencies in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,24 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
+    warmup: {
+      clientFiles: [
+        './src/App.jsx',
+        './src/main.jsx', 
+        './src/components/Sidebar.jsx'
+      ],
+    }
   },
+  optimizeDeps: {
+    include: [
+      'react', 
+      'react-dom', 
+      'react-router-dom', 
+      'lucide-react', 
+      'react-big-calendar',
+      'date-fns',
+      'axios'
+    ],
+    holdUntilCrawlEnd: false,
+  }
 })
